a03b0e9dbd8a3fd2bd1ad0a3bf4a3031c89377e9
Pratiksh <pratikshasdighe08@gmail.com>
2022-08-08 12:15:12 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+// import logo from "./logo.svg";
+import "./App.css";
+import InputField from "./compoenet/InputField";
+import TodoList from "./compoenet/TodoList";
+// import { Todo } from "./model";
+import {
+  selectTodo,
+  add,
+  selectCompleted,
+  drag,
+  dragcomplete,
+  dragcompletetodo,
+  dragactive,
+} from "./Redux/store";
 
-function App() {
+// import { DragDropContext } from "react-beautiful-dnd";
+
+const App: React.FC = () => {
+  let todos = useSelector(selectTodo);
+  console.log(todos);
+  let completedTodo = useSelector(selectCompleted);
+
+  const dispatch = useDispatch();
+  // const newTodoRef = useRef<HTMLInputElement>(null);
+  // const [todo, setTodo] = useState<string>("");
+  // const [todos, setTodos] = useState<Todo[]>([]);
+  // const [completedTodo, setCompletedTodo] = useState<Todo[]>([]);
+
+  const handleAdd = (e: any, inputRef: React.RefObject<HTMLInputElement>) => {
+    e.preventDefault();
+    if (inputRef.current) {
+      dispatch(add(inputRef.current.value));
+      // setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      // setTodo("");
+    }
+  };
+  const onDragEnd = (result: DropResult) => {
+    const { destination, source } = result;
+    console.log(result);
+    if (!destination) {
+      return;
+    }
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+    let add,
+      active = todos,
+      complete = completedTodo;
+
+    if (source.droppableId === "TodosList") {
+      add = active[source.index];
+      // console.log("1", add);
+      const index = source.index;
+      dispatch(drag(index));
+      // active.splice(source.index, 1);
+
+      // active.splice(source.index, 1);
+    } else {
+      add = complete[source.index];
+      // console.log("2", add);
+      // complete.splice(source.index, 1);
+      dispatch(dragcompletetodo(source.index));
+    }
+    if (destination.droppableId === "TodosList") {
+      // active.splice(destination?.index, 0, add);
+      const index = destination?.index;
+      dispatch(dragactive({ index, add }));
+    } else {
+      // complete.splice(destination?.index, 0, add);
+      const index = destination?.index;
+      dispatch(dragcomplete({ index, add }));
+    }
+    // completedTodo = complete;
+    console.log("completed", completedTodo);
+    // todos = active;
+    console.log("active", todos);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="App">
+        <span className="heading">TASKIFY</span>
+        {/* <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} /> */}
+        {/* {todos.map((t) => (
+        <li>{t.todo}</li>
+      ))} */}
+        <InputField handleAdd={handleAdd} />
+
+        {/* /> */}
+        <TodoList
+          // todo={todos}
+          todo={todos}
+          // todos={todos}
+          // setTodos={setTodos}
+          // completedTodo={completedTodo}
+          // setCompletedTodo={setCompletedTodo}
+        />
+      </div>
+    </DragDropContext>
   );
-}
+};
 
 export default App;
